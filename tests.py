a5e25f52ea01ad2f9e090cd6629ee356fbf3b3b6
PereBal <balaguerpere@gmail.com>
2015-07-30 19:41:52 +0200

    Rename changes made

--- tests.py
+++ tests.py
@@ -1,48 +1,48 @@
 import unittest
 
-import comparador as c
+import dictcompy as dcm
 
-class TestComparator(unittest.TestCase):
+class TestDictComPy(unittest.TestCase):
     def test_basics(self):
-        cm = c.Comparator({'a': int})
+        cm = dcm.DictComPy({'a': int})
         self.assertFalse(cm.match({'b': 1}))
         self.assertFalse(cm.match({'a': 1.0}))
         self.assertTrue(cm.match({'a': 1}))
 
-        cm = c.Comparator({'a': str})
+        cm = dcm.DictComPy({'a': str})
         self.assertFalse(cm.match({'b': u'a'}))
         self.assertFalse(cm.match({'a': 23}))
         self.assertTrue(cm.match({'a': 'a'}))
 
-        cm = c.Comparator({'a': {'b': int}})
+        cm = dcm.DictComPy({'a': {'b': int}})
         self.assertFalse(cm.match({'b': {'b': 1}}))
         self.assertFalse(cm.match({'a': {'a': 1}}))
         self.assertFalse(cm.match({'a': {'b': ''}}))
         self.assertTrue(cm.match({'a': {'b': 1}}))
 
     def test_In(self):
-        cm = c.Comparator({'a': (c.In, range(0,5,2))})
+        cm = dcm.DictComPy({'a': (dcm.In, range(0,5,2))})
         self.assertFalse(cm.match({'a': 1}))
         self.assertFalse(cm.match({'a': ''}))
         self.assertFalse(cm.match({'a': 10}))
         self.assertTrue(cm.match({'a': 2}))
 
     def test_Like(self):
-        cm = c.Comparator({'a': (c.Like, '\d,\w+')})
+        cm = dcm.DictComPy({'a': (dcm.Like, '\d,\w+')})
         self.assertFalse(cm.match({'a': '1,'}))
         self.assertFalse(cm.match({'a': ''}))
         self.assertFalse(cm.match({'a': '10'}))
         self.assertTrue(cm.match({'a': '1,0102dsada_dede'}))
 
     def test_Date(self):
-        cm = c.Comparator({'a': (c.Date, '%Y-%d-%m')})
+        cm = dcm.DictComPy({'a': (dcm.Date, '%Y-%d-%m')})
         self.assertFalse(cm.match({'a': '2015-02-31'}))
         self.assertFalse(cm.match({'a': ''}))
         self.assertFalse(cm.match({'a': '2015-32-01'}))
         self.assertTrue(cm.match({'a': '2015-30-01'}))
 
     def test_Excl(self):
-        cm = c.Comparator({'a': (c.Excl, ('1', '2'), {'1': int, '2': str})})
+        cm = dcm.DictComPy({'a': (dcm.Excl, ('1', '2'), {'1': int, '2': str})})
         self.assertFalse(cm.match({'a': {'1': 's'}}))
         self.assertFalse(cm.match({'a': {'2': 1}}))
         self.assertFalse(cm.match({'a': {'1': 1, '2': 's'}}))
