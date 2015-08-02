d7b71a9fd067638532a4e874cface4c736c69844
PereBal <balaguerpere@gmail.com>
2015-08-02 16:28:32 +0200

    Minor fixes, refactoring, tests added

--- python2.7/dictcompy.py
+++ python2.7/dictcompy.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 u"""
 Python dictionary/list comparator, take a look at the readme to underestand
 better how it works and what it does.
@@ -62,8 +61,8 @@ cfunc description:
 
     ·Eq: test if expr field is equal to celem.
 
-    ·Excl: test if celem contains only one of cexcl key fields. Has no effect on
-           lists.
+    ·Excl: test if celem contains only one of cexcl key fields. Has no effect
+           on lists.
 
 Version: 0.1
 
@@ -79,71 +78,126 @@ def Evalf(expr, model):
     _dispatch(expr, model[2], model[1])
 
 def Opt(expr, model):
+    u"""
+    Mark the left associated dictionary key as optional
+    """
     _dispatch(expr, model[1], model[2])
 
 def Eq(expr, model):
+    u"""
+    Test if the expression is equal (literaly) to the model
+    """
     if expr != model[1]:
-        raise DCPNonEqualException()
+        raise DCPNoMatchException()
 
 def Excl(expr, items):
+    u"""
+    Test if the listed fields are mutually exclusive on the expression and keep
+    comparing through the remaining field.
+    """
     r = tuple(set(items[1]) & set(expr))
     if len(r) != 1:
-        raise DCPNonEqualException()
+        raise DCPNoMatchException()
     _dispatch(expr, dict.fromkeys(r[0], items[2][r[0]]), items[3])
 
 def In(expr, iset):
+    u"""
+    Test if expression is on the listed set
+    """
     if expr not in iset[1]:
-        raise DCPNonEqualException()
+        raise DCPNoMatchException()
 
 def Like(expr, pattern):
+    u"""
+    Match the expression field against a regex pattern to see if they come along
+    """
     pat = re.compile(pattern[1])
     if not pat.match(expr):
-        raise DCPNonEqualException()
+        raise DCPNoMatchException()
 
 def Date(expr, form):
+    u"""
+    Match the expression against a date format to see if it fits, aka, it's 
+    equally formatted
+    """
     try:
         strptime(expr, form[1])
     except ValueError:
-        raise DCPNonEqualException()
+        raise DCPNoMatchException()
 
-def _type(field):
+def _field_type(field):
+    u"""
+    Returns the type of the field and the relevant part of the field itself. 
+    That part it's only used in 'sets(list, set, ...)' or dicts as is the next
+    part of the model that must be evaluated (specially needed with tuples
+    """
     if isinstance(field, tuple):
-        return field[0]
+        return (field[0], field[1])
     elif isinstance(field, type):
-        return field
+        return (field, field)
     else:
-        return type(field)
+        return (type(field), field)
 
 def _mandatory_key(field):
+    u"""
+    Test if a key is mandatory (aka, its not Opt)
+    """
     return not (isinstance(field, tuple) and
                 field[0] == Opt)
 
+def _valid_type(expr, model, type_):
+    u"""
+    It's a warpper to allow the use of keywords to match lists, dicts, etc and
+    guarantee a correct identification of matching elements even if an empty 
+    tuple-list-dict is provided
+    """
+    # type(expr) != t it's used to avoid true matches when empty dictionary
+    # or list,tuple,set is on the model
+    if type(expr) != type_:
+        raise DCPNoMatchException()
+    else:
+        # the model has a 'dict', 'list', 'set', ... keyword. The only check
+        # needed is the one we made before
+        return isinstance(model, type)
+
 module = modules[__name__]
 def _dispatch(expr, model, evalf):
-    t = _type(field=model)
+    u"""
+    This function picks the necessary evaluation/call (based on the type of the
+    field) and executes it
+    """
+    type_, field = _field_type(field=model)
 
-    if t == dict:
-        _compare_dict(expr, model, evalf)
-    elif t in (list, tuple, set):
-        _compare_list(expr, model, evalf)
-    elif getattr(module, t.__name__, None):
-        t(expr, (model + (evalf,)))
-    # TODO
-    elif isinstance(t, basestring):
+    if type_ == dict:
+        if not _valid_type(expr, model, type_):
+            _compare_dict(expr, field, evalf)
+    elif type_ in (list, tuple, set):
+        if not _valid_type(expr, model, type_):
+            _compare_list(expr, field, evalf)
+    elif getattr(module, type_.__name__, None):
+        # evalf might be used on t to call another _dispatch with diferent
+        # paramethers
+        type_(expr, (model + (evalf,)))
+    elif issubclass(type_, basestring):
         # empty strings evaluate falsly so if the evalf test has failed, it
         # might be due to an empty string. Thats the reason behind the 'and
         # expr' part
         if not evalf(expr, basestring) and expr:
-            raise DCPNonEqualException()
+            raise DCPNoMatchException()
     else:
-        if not evalf(expr, t):
-            raise DCPNonEqualException()
+        # type(expr) != t -> it's necessary to avoid true match when integer
+        # and boolean (for example) are evaluated through isinstance
+        if type(expr) != type_ or not evalf(expr, type_):
+            raise DCPNoMatchException()
 
 def _compare_dict(expr, model, evalf):
+    u"""
+    Compare expr vs model dicts field by field, recursively if needed
+    """
     for k in model.keys():
         if not k in expr:
             if _mandatory_key(field=model[k]):
-                raise DCPNonEqualException()
+                raise DCPNoMatchException()
             else:
                 continue
 
@@ -151,9 +205,13 @@ def _compare_dict(expr, model, evalf):
 
 
 def _compare_list(expr, model, evalf):
+    u"""
+    Compare expr vs model lists, tuples, sets field by field, recursively if 
+    needed
+    """
     if len(model) != len(expr):
-        if len(model) != 1:
-            raise DCPNonEqualException()
+        if len(model) != 1 or len(expr) < len(model):
+            raise DCPNoMatchException()
 
         iterset = expr
         ITERINC = 0
@@ -167,18 +225,29 @@ def _compare_list(expr, model, evalf):
         model_key = model_key + ITERINC
 
 class DCPException(Exception):
-
+    u"""
+    Just a user defined exception class. It's used to raise internal errors
+    """
     def __init__(self, *args, **kwargs):
         super(DCPException, self).__init__(*args, **kwargs)
 
-class DCPNonEqualException(Exception):
+class DCPNoMatchException(Exception):
+    u"""
+    Just a user defined exception class. It's used to raise a 'non matching'
+    element
+    """
 
     def __init__(self, *args, **kwargs):
-        super(DCPNonEqualException, self).__init__(*args, **kwargs)
+        super(DCPNoMatchException, self).__init__(*args, **kwargs)
 
 class DictComPy(object):
 
     def __init__(self, model, evalf=None):
+        u"""
+        model: dictionary or list that will be used as reference when matching.
+        evalf: isinstance alternative function to apply on fields to see if 
+               they match.
+        """
         super(DictComPy, self).__init__()
 
         if evalf:
@@ -202,19 +271,23 @@ class DictComPy(object):
         return self._model
 
     def match(self, expr):
+        u"""
+        Test if expression is equal (equality test made with the evalf) to model
+        Return True on equality False otherwise
+        """
         if not isinstance(expr, (dict, list)):
             raise DCPException('The expression MUST be a dictionary or a list')
 
         try:
             if isinstance(expr, dict):
                 if not isinstance(self._model, dict):
-                    raise DCPNonEqualException()
+                    raise DCPNoMatchException()
                 _compare_dict(expr, self._model, self._evalf)
             else:
                 if not isinstance(self._model, list):
-                    raise DCPNonEqualException()
+                    raise DCPNoMatchException()
                 _compare_list(expr, self._model, self._evalf)
-        except DCPNonEqualException:
+        except DCPNoMatchException:
             return False
         else:
             return True
--- python2.7/tests.py
+++ python2.7/tests.py
@@ -1,32 +1,136 @@
+# TODO
+# model: (int, 1) -> funciona com toca. Les tuples son especials x)
+# faltaria afegir un parseig del model
 import unittest
 
 import dictcompy as dcm
 
 class TestDictComPy(unittest.TestCase):
-    def test_basics(self):
+    def test_plain_int(self):
         cm = dcm.DictComPy({'a': int})
         self.assertFalse(cm.match({'b': 1}))
         self.assertFalse(cm.match({'a': 1.0}))
+        self.assertFalse(cm.match({'a': True}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 'as'}))
         self.assertTrue(cm.match({'a': 1}))
+        self.assertTrue(cm.match({'a': 0}))
 
+    def test_plain_float(self):
+        cm = dcm.DictComPy({'a': float})
+        self.assertFalse(cm.match({'b': 1.0}))
+        self.assertFalse(cm.match({'a': 0}))
+        self.assertFalse(cm.match({'a': True}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 'as'}))
+        self.assertTrue(cm.match({'a': 1.0}))
+
+    def test_plain_complex(self):
+        cm = dcm.DictComPy({'a': complex})
+        c = complex(1, 2)
+        self.assertFalse(cm.match({'b': c}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': 1.0}))
+        self.assertFalse(cm.match({'a': True}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 'as'}))
+        self.assertTrue(cm.match({'a': c}))
+
+    def test_plain_string(self):
         cm = dcm.DictComPy({'a': str})
         self.assertFalse(cm.match({'b': u'a'}))
         self.assertFalse(cm.match({'a': 23}))
+        self.assertFalse(cm.match({'a': True}))
         self.assertTrue(cm.match({'a': 'a'}))
         self.assertTrue(cm.match({'a': u'a'}))
+        self.assertTrue(cm.match({'a': u''}))
+        self.assertTrue(cm.match({'a': ''}))
 
+    def test_plain_list(self):
+        cm = dcm.DictComPy({'a': list})
+        self.assertFalse(cm.match({'b': []}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertTrue(cm.match({'a': []}))
+        self.assertTrue(cm.match({'a': [1]}))
+        self.assertTrue(cm.match({'a': [1, '1021', True]}))
+
+        cm = dcm.DictComPy({'a': []})
+        self.assertFalse(cm.match({'b': []}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': [1]}))
+        self.assertFalse(cm.match({'a': [1, '1021', True]}))
+        self.assertTrue(cm.match({'a': []}))
+
+        cm = dcm.DictComPy({'a': (list, [])})
+        self.assertFalse(cm.match({'b': []}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': [1]}))
+        self.assertFalse(cm.match({'a': [1, '1021', True]}))
+        self.assertTrue(cm.match({'a': []}))
+
+        cm = dcm.DictComPy({'a': [int]})
+        self.assertFalse(cm.match({'b': [1]}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': []}))
+        self.assertFalse(cm.match({'a': [1, '1021', True]}))
+        self.assertTrue(cm.match({'a': [1]}))
+        self.assertTrue(cm.match({'a': [1, 2, 3]}))
+
+        cm = dcm.DictComPy({'a': [int, str, bool]})
+        self.assertFalse(cm.match({'b': []}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': []}))
+        self.assertFalse(cm.match({'a': [1]}))
+        self.assertFalse(cm.match({'a': [1, '1021', True, 1.0]}))
+        self.assertTrue(cm.match({'a': [1, '1021', True]}))
+
+    def test_plain_tuple(self):
+        cm = dcm.DictComPy({'a': tuple})
+        self.assertFalse(cm.match({'b': ()}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertTrue(cm.match({'a': ()}))
+        self.assertTrue(cm.match({'a': (1,)})) # 1, <-- common mistake
+        self.assertTrue(cm.match({'a': (1, '1021', True)}))
+
+        cm = dcm.DictComPy({'a': (tuple, (int,))}) # ',' <--
+        self.assertFalse(cm.match({'b': (1,)}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': ()}))
+        self.assertFalse(cm.match({'a': (1, '1021', True)}))
+        self.assertTrue(cm.match({'a': (1,)}))
+        self.assertTrue(cm.match({'a': (1, 2, 3)}))
+
+        cm = dcm.DictComPy({'a': (tuple, (int,str,bool))}) # ',' <--
+        self.assertFalse(cm.match({'b': (1,)}))
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 1}))
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': ()}))
+        self.assertFalse(cm.match({'a': (1,)}))
+        self.assertFalse(cm.match({'a': (1, 2, 3)}))
+        self.assertFalse(cm.match({'a': (1, '1021', True, 1.0)}))
+        self.assertTrue(cm.match({'a': (1, '1021', True)}))
+
+    def test_nested_int(self):
         cm = dcm.DictComPy({'a': {'b': int}})
         self.assertFalse(cm.match({'b': {'b': 1}}))
         self.assertFalse(cm.match({'a': {'a': 1}}))
         self.assertFalse(cm.match({'a': {'b': ''}}))
         self.assertTrue(cm.match({'a': {'b': 1}}))
-
-        cm = dcm.DictComPy({'a': list})
-        self.assertFalse(cm.match({'a': ''}))
-        self.assertFalse(cm.match({'a': 1}))
-        self.assertFalse(cm.match({'a': {}}))
-        self.assertFalse(cm.match({'a': [1]}))
-        self.assertTrue(cm.match({'a': []}))
 
     def test_In(self):
         cm = dcm.DictComPy({'a': (dcm.In, range(0,5,2))})
@@ -60,6 +164,10 @@ class TestDictComPy(unittest.TestCase):
 
     def test_Opt(self):
         cm = dcm.DictComPy({'a': (dcm.Opt, int)})
+        self.assertFalse(cm.match({'a': ''}))
+        self.assertFalse(cm.match({'a': 's'}))
+        self.assertFalse(cm.match({'a': True}))
+        self.assertFalse(cm.match({'a': 1.0}))
         self.assertTrue(cm.match({'a': 1}))
         self.assertTrue(cm.match({'b': 1}))
 
@@ -68,6 +176,11 @@ class TestDictComPy(unittest.TestCase):
         self.assertFalse(cm.match({'a': 1}))
         self.assertFalse(cm.match({'a': 4}))
         self.assertTrue(cm.match({'a': 3}))
+
+        cm = dcm.DictComPy({'a': (dcm.Eq, dict)})
+        self.assertFalse(cm.match({'a': {}}))
+        self.assertFalse(cm.match({'a': list}))
+        self.assertTrue(cm.match({'a': dict}))
 
 if __name__ == '__main__':
     unittest.main()
